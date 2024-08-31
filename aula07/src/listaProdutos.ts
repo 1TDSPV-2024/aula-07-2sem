eb9a45c347c9709b7f95c10201a15fa23216bae9
andremarko <andre.marcolongo@gmail.com>
2024-08-30 22:34:57 -0300

    Passagem de params. Hook useParams. Destructuring. Listagem externa e recuperação de dados.

---
+++ aula07/src/listaProdutos.ts
@@ -0,0 +1,18 @@
+type ObjetoLista = {
+    id: number;
+    nome: string;
+    qtd: number;
+    preco: number;
+}
+
+export const listaProdutos: ObjetoLista[] = [
+
+    { id: 1, nome: "Mouse", qtd: 10, preco: 78 },
+    { id: 2, nome: "Monitor", qtd: 3, preco: 2378 },
+    { id: 3, nome: "Teclado", qtd: 12, preco: 145 },
+    { id: 4, nome: "Cadeira", qtd: 2, preco: 3479 },
+    { id: 5, nome: "Mousepad", qtd: 100, preco: 25 }
+
+]
+
+
--- aula07/src/routes/EditarProdutos/index.tsx
+++ aula07/src/routes/EditarProdutos/index.tsx
@@ -1,23 +1,37 @@
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"; // pacote react router dom -> só trata parâmetros
+import { listaProdutos } from "../../listaProdutos";
 
-export default function EditarProdutos(){
+// hooks são funções que trabalham com ações específicas -> a maioria usa 'use'. -> 
+// você pode o useParams agrega todos os parametros que é enviado para ela -> destructuring
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "EDITAR PRODUTOS";
+export default function EditarProdutos() {
 
-      //Realizando um destructuring para recuperar o parâmetro que foi passado na rota:
-      //Ex: Se a rota criada foi: /meus-dados/:dados
-      //O destructuring irá recuperar o dado que foi passado na rota e atribuir a propriedade de um elemento cuja o nome é aquele criado na rota, antes do dois pontos,ou seja, (:dados)
-      //Então teriamos que realizar a seguinte ação para receber esta informação.
-      // const{dados} = useParams(), um detalhe aqui é que o useParams() pertence ao react-router e deve ser importado dele
-      const {id} = useParams();
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "EDITAR PRODUTOS";
 
-      return(
+  //Realizando um destructuring para recuperar o parâmetro que foi passado na rota:
+  //Ex: Se a rota criada foi: /meus-dados/:dados
+  //O destructuring irá recuperar o dado que foi passado na rota e atribuir a propriedade de um elemento cuja o nome é aquele criado na rota, antes do dois pontos,ou seja, (:dados)
+  //Então teriamos que realizar a seguinte ação para receber esta informação.
+  // const{dados} = useParams(), um detalhe aqui é que o useParams() pertence ao react-router e deve ser importado dele
+
+  const { id } = useParams(); // função hook react
+
+  const objProduto = listaProdutos.find((prod) => prod.id == Number(id));
+
+
+
+  return (
+    <div>
+      <h1>Olá, mundo sou o EditarProdutos!</h1>
       <div>
-        <h1>Olá, mundo sou o EditarProdutos!</h1>
+        <h2>ID: {id}</h2>
         <div>
-          <h2>ID: {id}</h2>
+          <p>Nome: {objProduto?.nome}</p>
+          <p>Quantidade: {objProduto?.qtd}</p>
+          <p>Preço: R$ {objProduto?.preco}</p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
