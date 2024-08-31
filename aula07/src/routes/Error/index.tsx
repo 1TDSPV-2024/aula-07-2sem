a44611e9fabbd3816c51f1b3fd8f77b3786a8456
andremarko <andre.marcolongo@gmail.com>
2024-08-30 22:49:08 -0300

    página 404 com aleatoriedade , utilizando template string

--- aula07/src/routes/Error/index.tsx
+++ aula07/src/routes/Error/index.tsx
@@ -1,13 +1,20 @@
+{/* PASTA PUBLIC É UMA PASTA LÓGICA */ }
 
+export default function Error() {
 
-export default function Error(){
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "Erro 404";
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "Erro 404";
+  const imgPathNumber = Math.floor(Math.random() * 3) + 1;
 
-    return(
+  return (
+    <div>
+      <h1>ERRO 404 - PÁGINA NÃO ENCONTRADA.</h1>
       <div>
-        <h1>Olá, mundo sou o Error!</h1>
+        <figure>
+          <img src={`/img404/img-${imgPathNumber}-404.jpg`} alt="Error-404" />
+        </figure>
       </div>
-    );
-  }
+    </div>
+  );
+}
