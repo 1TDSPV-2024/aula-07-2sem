9fe7de7e0b0ac4b7d834427f69d39465ea05e4eb
rtof06 <ricardoofilho06@gmail.com>
2024-08-30 22:35:37 -0300

    Passagem de parametros, Hook useParams

---
+++ aula07/src/listaProdutos.ts
@@ -0,0 +1,40 @@
+type ObjetoLista = {
+    id: number,
+    nome: string,
+    qtd: number,
+    preco: number
+}
+
+export const listaProdutos: ObjetoLista[] = [
+
+    {
+        id: 1,
+        nome: 'Mouse',
+        qtd: 10,
+        preco: 78.99
+    },
+    {
+        id: 2,
+        nome: 'Monitor',
+        qtd: 10,
+        preco: 2699.99
+    },
+    {
+        id: 3,
+        nome: 'Teclado',
+        qtd: 10,
+        preco: 199.99
+    },
+    {
+        id: 4,
+        nome: 'Cadeira',
+        qtd: 10,
+        preco: 409.99
+    },
+    {
+        id: 5,
+        nome: 'Mousepad',
+        qtd: 10,
+        preco: 89.99
+    }
+]
